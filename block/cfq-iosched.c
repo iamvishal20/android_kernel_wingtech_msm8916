6705c6ed4bc0a6e37fff26161b5fd601e8e341c2
Hidehiro Kawai <hidehiro.kawai.ez@hitachi.com>
2016-11-25 10:16:51 +0530

    cfq-iosched: fix incorrect filing of rt async cfqq
    Signed-off-by: dev_harsh1998 <harshitjain6751@gmail.com>

--- block/cfq-iosched.c
+++ block/cfq-iosched.c
@@ -3655,6 +3655,11 @@ cfq_get_queue(struct cfq_data *cfqd, bool is_sync, struct cfq_io_cq *cic,
 	struct cfq_queue *cfqq = NULL;
 
 	if (!is_sync) {
+		if (!ioprio_valid(cic->ioprio)) {
+			struct task_struct *tsk = current;
+			ioprio = task_nice_ioprio(tsk);
+			ioprio_class = task_nice_ioclass(tsk);
+		}
 		async_cfqq = cfq_async_queue_prio(cfqd, ioprio_class, ioprio);
 		cfqq = *async_cfqq;
 	}
