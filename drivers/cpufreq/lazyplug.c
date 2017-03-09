ae76a2cbed5664eefdf1515415f376b1e86419d3
Prakharluv <prakhar.dc@gmail.com>
2017-03-09 15:43:53 +0000

    enable lazyplug by defualt and remove touch boost

--- drivers/cpufreq/lazyplug.c
+++ drivers/cpufreq/lazyplug.c
@@ -98,10 +98,10 @@ static struct delayed_work lazyplug_boost;
 static struct workqueue_struct *lazyplug_wq;
 static struct workqueue_struct *lazyplug_boost_wq;
 
-static unsigned int __read_mostly lazyplug_active = 0;
+static unsigned int __read_mostly lazyplug_active = 1;
 module_param(lazyplug_active, uint, 0664);
 
-static unsigned int __read_mostly touch_boost_active = 1;
+static unsigned int __read_mostly touch_boost_active = 0;
 module_param(touch_boost_active, uint, 0664);
 
 static unsigned int __read_mostly nr_run_profile_sel = 0;
