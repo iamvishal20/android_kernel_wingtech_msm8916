116f56efebe0b967b3bfa0ef8a90d71cd1ed899f
Harsh Shandilya <msfjarvis@gmail.com>
2017-03-30 14:06:42 +0530

    Resolve mismerge of fdf04d7cbf9c8449ea7b4201633a065b9e012325
    Change-Id: I7f2c54b58183b4c28ca49bbc4dded2de839b1b71

--- net/core/fib_rules.c
+++ net/core/fib_rules.c
@@ -657,12 +657,6 @@ static int fib_nl_fill_rule(struct sk_buff *skb, struct fib_rule *rule,
 	     nla_put_uid_range(skb, &rule->uid_range)))
 		goto nla_put_failure;
 
-	if (uid_valid(rule->uid_start))
-	     nla_put_uid(skb, FRA_UID_START, rule->uid_start);
-
-	if (uid_valid(rule->uid_end))
-	     nla_put_uid(skb, FRA_UID_END, rule->uid_end);
-
 	if (ops->fill(rule, skb, frh) < 0)
 		goto nla_put_failure;
 
